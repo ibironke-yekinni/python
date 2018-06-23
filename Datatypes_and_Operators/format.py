46a0a041f9986e1b9a126f601427450151f0aec7
Ibironke Yekinni <35920144+ykthinkbig@users.noreply.github.com>
2018-06-24 00:03:49 +0100

    Create format.py

---
+++ Datatypes_and_Operators/format.py
@@ -0,0 +1,11 @@
+'''
+format() Practice
+Use the coding space below to practice using the format() string method. There are no right or wrong answers here, just practice!
+'''
+
+# Write two lines of code below, each assigning a value to a variable
+to = "study"
+do = "python"
+# Now write a print statement using .format() to print out a sentence and the 
+#   values of both of the variables
+print("I love to {} {}".format(to,do))
